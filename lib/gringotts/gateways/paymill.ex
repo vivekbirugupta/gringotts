dd88bfc82977ffa1fa499ec703a9ddd553c1596d
Sagar Karwande <sagar_karwande123@yahoo.com>
2017-12-27 20:39:45 +0530

    Added paymill authorize mock test

--- lib/gringotts/gateways/paymill.ex
+++ lib/gringotts/gateways/paymill.ex
@@ -59,7 +59,6 @@ defmodule Gringotts.Gateways.Paymill do
   """
   @spec authorize(number, String.t | CreditCard.t, Keyword) :: {:ok | :error, Response}
   def authorize(amount, card_or_token, options) do
-    Keyword.put(options, :money, amount)
     action_with_token(:authorize, amount, card_or_token, options)
   end
 
@@ -183,7 +182,7 @@ defmodule Gringotts.Gateways.Paymill do
   end
 
   defp add_amount(post, money, options) do
-    post ++ [{"amount", money}, {"currency", @default_currency}]
+    post ++ [{"amount", money}, {"currency", get_currency(options)}]
   end
 
   defp set_username(options) do
@@ -323,12 +322,15 @@ defmodule Gringotts.Gateways.Paymill do
 
     def parse({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
       body = Poison.decode!(body)
-      parse_body(body)
+      [status_code: 200]
+      |> parse_body(body)
     end
     def parse({:ok, %HTTPoison.Response{body: body, status_code: 400}}) do
       body = Poison.decode!(body)
-      []
+      [status_code: 400, success: false]
       |> set_params(body)
+      |> handle_error(body)
+      |> handle_opts
     end
     def parse({:ok, %HTTPoison.Response{body: body, status_code: 404}}) do
       body = Poison.decode!(body)
@@ -341,12 +343,20 @@ defmodule Gringotts.Gateways.Paymill do
     defp set_success(opts, %{"error" => error}) do
       opts ++ [message: error, success: false]
     end
+    defp set_success(opts, %{"status" => "failed"}) do
+      opts ++ [success: false]
+    end
     defp set_success(opts, %{"transaction" => %{ "response_code" => 20000}}) do
       opts ++ [success: true]
     end
 
-    defp parse_body(%{"data" => data}) do
-      []
+    defp handle_error(opts, %{"error" => %{"messages" => messages}}) do
+      [{key, msg}| tail] = Map.to_list(messages)
+      opts ++ [message: msg]
+    end
+
+    defp parse_body(opts, %{"data" => data}) do
+      opts
       |> set_success(data)
       |> parse_authorization(data)
       |> parse_status_code(data)
@@ -365,16 +375,16 @@ defmodule Gringotts.Gateways.Paymill do
     defp parse_status_code(opts, %{"status" => "failed"} = body) do
       response_code = get_in(body, ["transaction", "response_code"])
       response_msg = Map.get(@response_code, response_code, -1)
-      opts ++ [message: response_msg]
+      opts ++ [error_code: response_code, message: response_msg]
     end
     defp parse_status_code(opts, %{ "transaction" => transaction}) do
       response_code = Map.get(transaction, "response_code", -1)
       response_msg = Map.get(@response_code, response_code, -1)
-      opts ++ [status_code: response_code, message: response_msg]
+      opts ++ [error_code: response_code, message: response_msg]
     end
     defp parse_status_code(opts, %{"response_code" => code}) do
       response_msg = Map.get(@response_code, code, -1)
-      opts ++ [status_code: code, message: response_msg]
+      opts ++ [error_code: code, message: response_msg]
     end
 
     #Authorization
